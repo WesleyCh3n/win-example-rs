ebd6d735c5a1c11fdb97edceccc314d2fa7e0a92
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-11-03 12:15:07 +0800

    add: device-info

---
+++ device-info/src/main.rs
@@ -0,0 +1,76 @@
+use serde::{Deserialize, Serialize};
+use windows::core::{Result, PCWSTR};
+use windows::Win32::Devices::DeviceAndDriverInstallation::*;
+
+#[derive(Serialize, Deserialize)]
+struct DeviceInfo {
+    class: String,
+    enumerator: String,
+    description: String,
+    manufacturer: String,
+    hardware_id: String,
+    compatible_id: String,
+    class_guid: String,
+}
+
+fn main() {
+    unsafe {
+        match get_info() {
+            Ok(s) => println!("{}", s),
+            Err(e) => println!("{}", e),
+        }
+    }
+}
+
+unsafe fn get_info() -> Result<String> {
+    let h_dev_info = SetupDiGetClassDevsW(
+        None,
+        None,
+        None,
+        DIGCF_PRESENT | DIGCF_ALLCLASSES,
+    )?;
+    let mut dev_info_data = SP_DEVINFO_DATA::default();
+    dev_info_data.cbSize = std::mem::size_of::<SP_DEVINFO_DATA>() as u32;
+    let mut buf: [u8; 1024] = [0; 1024];
+    let mut reqsize = 0;
+    macro_rules! get_property {
+        ($p:expr) => {
+            if SetupDiGetDeviceRegistryPropertyW(
+                h_dev_info,
+                &mut dev_info_data,
+                $p,
+                None,
+                Some(&mut buf),
+                Some(&mut reqsize),
+            )
+            .as_bool()
+            {
+                PCWSTR(buf.as_ptr().cast::<u16>())
+                    .to_string()
+                    .expect("failed to construct pcwstr")
+            } else {
+                "".to_string()
+            }
+        };
+    }
+
+    let mut i = 0;
+    let mut final_output: Vec<DeviceInfo> = Vec::new();
+    while SetupDiEnumDeviceInfo(h_dev_info, i, &mut dev_info_data).as_bool() {
+        final_output.push(DeviceInfo {
+            class: get_property!(SPDRP_CLASS),
+            enumerator: get_property!(SPDRP_ENUMERATOR_NAME),
+            description: get_property!(SPDRP_DEVICEDESC),
+            manufacturer: get_property!(SPDRP_MFG),
+            hardware_id: get_property!(SPDRP_HARDWAREID),
+            compatible_id: get_property!(SPDRP_COMPATIBLEIDS),
+            class_guid: get_property!(SPDRP_CLASSGUID),
+        });
+        i += 1;
+    }
+
+    SetupDiDestroyDeviceInfoList(h_dev_info);
+
+    Ok(serde_json::to_string_pretty(&final_output)
+        .unwrap_or("construct json string failed".into()))
+}
